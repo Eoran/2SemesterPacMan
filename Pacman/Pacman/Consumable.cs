593fc8bbf62b45278f4a8b2a6b50483c210ab3cf
Brian <briabn@fiberflex.dk>
2015-05-29 10:54:14 +0200

    Har lavet Consumable

---
+++ Pacman/Pacman/Consumable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pacman
+{
+    class Consumable
+    {
+        public bool collected;
+
+        Random rnd = new Random();
+
+        protected bool type;
+
+        string[] consumables = new string[2] { @"card", @"book" };
+
+        public string RandomConsumable()
+        {
+            int randomConsumable = rnd.Next(consumables.Length);
+            collected = false;
+            return
+                consumables[randomConsumable];
+
+
+        }
+    }
+}
